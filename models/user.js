95bbb0ddb314a77afd574c2e685d63046be13357
Dhruv535-Fr <parmardhruv498@gmail.com>
2026-04-28 18:52:48 +0530

    Setting up Project-BlogApp

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const { Schema, default: mongoose, model } = require('mongoose');
+const {createHmac , randomBytes } = require('crypto')
+ 
+const userSchema = new Schema({
+    fullName : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    salt : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true,
+    },
+    profileImgUrl : {
+        type : String,
+        default : "/images/defaultAvatar.png"
+    },
+    role : {
+        type : String,
+        enum : ["USER","ADMIN"],
+        default : "USER",
+    }
+},
+{
+    timestamps : true
+});
+
+userSchema.pre("save", function (next) {
+    const user = this;
+    if(!user.isModified("password")) return;
+
+    const salt = randomBytes(16).toString();
+    const hashPassword = createHmac('sha256',salt).update(user.password).digest('hex');
+
+    this.salt = salt;
+    this.password = hashPassword;
+
+    next();
+});
+const User = model('user',userSchema);
+
+module.exports = {
+    User,
+}
